245f2d205f263f53ced487f82e8f032a1b022cc2
Pedro Militão <pmilitao@lenovo.com>
2025-12-18 13:24:54 -0300

    Classe abstrata para Algoritmo

--- classes/Algoritmo.py
+++ classes/Algoritmo.py
@@ -1,4 +1,7 @@
-class Algoritmo:
+from abc import ABC, abstractmethod
 
+class Algoritmo(ABC):
+
+    @abstractmethod
     def executa_algoritmo(self, cadeia_ref: list[int], total_quadros: int) -> dict:
-        print("Oops! Error")
+        pass
